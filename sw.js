7c53ec720aca9b824d7f82afe3243c79c3de1fdd
Idan Nahum <idanahum1122@gmail.com>
2026-02-07 09:13:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-v102';
+const CACHE_NAME = 'attendance-v103';
 const urlsToCache = [
   './index.html',
   './manifest.json',
@@ -18,7 +18,7 @@ const urlsToCache = [
 
 // Install event - cache essential files
 self.addEventListener('install', (event) => {
-  console.log('[SW] Installing Service Worker v102...');
+  console.log('[SW] Installing Service Worker v103...');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
@@ -122,7 +122,7 @@ self.addEventListener('fetch', (event) => {
 
 // Activate event - clean old caches
 self.addEventListener('activate', (event) => {
-  console.log('[SW] Activating Service Worker v102...');
+  console.log('[SW] Activating Service Worker v103...');
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -147,4 +147,4 @@ self.addEventListener('message', (event) => {
   }
 });
 
-console.log('[SW] Service Worker v102 loaded');
+console.log('[SW] Service Worker v103 loaded');
